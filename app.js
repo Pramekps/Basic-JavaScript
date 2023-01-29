e16a462efdf5f68bc7d2536256d245dc8965cd8e
Pramkps <ppm.utcha@gmail.com>
2023-01-29 21:23:02 +0700

    lotto

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("hello Prame")
+console.log("hello Prame")
+console.log("hello Prame")
+var age = (4+2*2/2*2+4+1-5+19)+91
+var name =  "Prame"
+var FirstName = "Utcha"
+var LastName = "Kruepunsak"
+console.log(age)  
+console.log(typeof age) 
+console.log(FirstName)
+console.log(LastName)
+//alert(FirstName +" "+ LastName)
+var x = 20
+{
+    var x = 2
+}
+let lotto_number = prompt("กรอกเลขที่ต้องการซื้อ")
+document.getElementById("result").innerHTML = Math.floor(Math.random()*100)
