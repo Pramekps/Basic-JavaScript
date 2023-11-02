888dc6db2663ae277496fd1c01bcfa4e7107d9a0
Utcha Kruepunsak <utcha.kru@metthier.co.th>
2023-11-02 17:07:22 +0700

    no message

--- epkong/ep5.js
+++ epkong/ep5.js
@@ -1,3 +1,13 @@
-const num = [100,200,300,400]
-const num2 = [100,...num]
-console.log(num2)
+function convertToOffsetTime(utcTimestamp) {
+    const utcDate = new Date(utcTimestamp);
+    utcDate.setHours(utcDate.getHours() + 7); // Add +7 hours offset
+    return utcDate.toISOString();
+  }
+  
+  // Example usage
+  const utcTimestamp = (new Date()).toISOString();
+  const localTimeWithOffset = convertToOffsetTime(utcTimestamp);
+  
+  console.log("UTC Timestamp:", utcTimestamp);
+  console.log("Local Time with +7 hours offset:", localTimeWithOffset);
+  
---
+++ epkong/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+console.log(0.1 + 0.2) // Result: 0.30000000000000004
+
+console.log(0.1 + 0.2 == 0.3) // Result: false
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+  sum += 0.1;
+}
+
+console.log(sum) // Result: 0.9999999999999999
